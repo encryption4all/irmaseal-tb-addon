3e4f87abe68bc2f7226bbf458216bf60b8525a57
Leon <leon.botros@ru.nl>
2021-06-08 14:58:56 +0200

    remove popups and messageDisplayActions in favour of messageDisplayScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CspHtmlWebpackPlugin = require('csp-html-webpack-plugin')
 
 const defaultMode = 'development'
 const outputPath = path.resolve(__dirname, './dist/release')
@@ -21,8 +19,6 @@ module.exports = [
         mode: defaultMode,
         entry: {
             background: './src/background/background.ts',
-            sealCompose: './src/components/composePopup/sealCompose.ts',
-            sealDecrypt: './src/components/decryptPopup/decryptPopup.ts',
         },
         experiments: { syncWebAssembly: true, topLevelAwait: true },
         output: { path: outputPath },
@@ -31,21 +27,6 @@ module.exports = [
             extensions: extensions,
             fallback: { http: false, https: false, url: false, util: false },
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                title: 'IRMAseal compose',
-                template: './src/components/composePopup/sealCompose.html',
-                filename: 'sealCompose.html',
-                chunks: ['sealCompose'],
-            }),
-            new HtmlWebpackPlugin({
-                title: 'IRMAseal decrypt',
-                template: './src/components/decryptPopup/decryptPopup.html',
-                filename: 'sealDecrypt.html',
-                chunks: ['sealDecrypt'],
-            }),
-            new CspHtmlWebpackPlugin(),
-        ],
     },
     {
         name: 'experiment',
